25ec66fd29d06612dddd4ab4e0dcc5af5c74a81e
Dane Potter <potterd123@gmail.com>
2019-01-21 22:40:03 -0800

    99% done - need array -dane

--- Wh3nsDay/ThirdViewController.swift
+++ Wh3nsDay/ThirdViewController.swift
@@ -13,16 +13,95 @@ import CoreData
 class ThirdViewController: UIViewController {
     private var currentDateInString = ""
     private var events = Array<[NSManagedObject]>()
+    private var events2 = [Int]()
+    private var scrollNumber = 0
+    private var scrollCount = 0
+    private var endI = 0
+    func addup(e: Int){
+        var i = 0
+        while i < e{
+            events2.append(i)
+            i = i + 1
+        }
+    }
     override func viewDidLoad() {
         super.viewDidLoad()
         print(currentDateInString)
-        events = getEventDay(n: currentDateInString)
-        print(events[0])
+        addup(e: 43)
+       // scrollNumber = Int(events2 / 12)
+       // events = getEventDay(n: currentDateInString)
+        //print(events[0])
         // Do any additional setup after loading the view, typically from a nib.
+        if(events2.count > 11){
+            endI = 11
+        } else { endI = events2.count - 1}
+        setup(startI: 0, endI: endI)
+        // setup(startI: 12, endI: 23)
+    }
+    @IBAction func swipeUp(_ sender: Any) {
+        scrollCount = scrollCount + 1
+        print(scrollCount)
+        if(12 * scrollCount  > events2.count){
+            print("done")
+            scrollCount = scrollCount - 1
+            return
+        }
         
+        endI = (events2.count) - (events2.count - (scrollCount * 12)) - 2
+        print("________________" + String(endI))
+        if(events2.count - endI > 11){
+            endI = 11 + scrollCount * 12
+        }else{
+            endI = events2.count % 12 + scrollCount * 12 - 1
+        }
+        print(endI)
+        
+        setup(startI: 12 * scrollCount, endI: endI)
+    }
+    @IBAction func swipeDown(_ sender: Any) {
+        scrollCount = scrollCount - 1
+        if(scrollCount < 0){
+           scrollCount = scrollCount + 1
+            return
+        }
+        endI = (events2.count) - (events2.count - (scrollCount * 12)) - 2
+        print("________________" + String(endI))
+        if(events2.count - endI > 11){
+            endI = 11 + scrollCount * 12
+        }else{
+            endI = events2.count % 12 + scrollCount * 12 - 1
+        }
+        setup(startI: 12 * scrollCount, endI: endI)
     }
     public func setCurrentDateInString(d: String){
         currentDateInString = d
+    }
+    func setup(startI: Int, endI: Int){
+        view.subviews.forEach({ $0.removeFromSuperview() })
+        let yDifference = (Int(UIScreen.main.bounds.height) - 250) / (endI - startI)
+        var i = endI
+        
+        while i >= startI {
+            let eventLabel = UILabel()
+            eventLabel.frame = CGRect(x: 207, y: 150
+                + (yDifference * (i % 12)), width: 300, height: 30)
+            eventLabel.center.x = self.view.center.x
+            print(i)
+            eventLabel.text = String(events2[i])
+            eventLabel.font = UIFont(name: "HelveticaNeue-Bold", size: 38)
+            eventLabel.textColor = UIColor.blue
+            eventLabel.textAlignment = NSTextAlignment.center
+            eventLabel.lineBreakMode = NSLineBreakMode.byWordWrapping
+            eventLabel.isHighlighted = false
+            eventLabel.isUserInteractionEnabled = true
+            eventLabel.isEnabled = true
+            eventLabel.numberOfLines = 0
+            eventLabel.adjustsFontSizeToFitWidth = true
+            eventLabel.baselineAdjustment = UIBaselineAdjustment.alignCenters
+            self.view.addSubview(eventLabel)
+            
+            i = i - 1
+        }
     }
     
     func getEventDay(n: String) -> Array<[NSManagedObject]> {
